07f3ed4a91cf0560a5ca2ae894ba020c306b2dd8
Andrés Felipe Losada luna <af.losada@uniandes.edu.co>
2021-07-20 15:43:04 -0500

    pre-finalizar react

---
+++ frontend/spellcheckerfront/src/components/List.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+function ListItemLink(props) {
+    return <ListItem button component="a" {...props} />;
+}
+
+export default class Lista extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            {items.map((item,index)=>{
+                return <li key={index}>{item}</li>
+            })}
+        );
+    }
+}
